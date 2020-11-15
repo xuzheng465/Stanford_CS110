3e52b7c044ff83d88cb4ac47f603d374460efe04
xuzheng465 <xuzheng465@gmail.com>
2020-11-15 08:34:37 -0800

    add right version of lab 1

--- lab1/nonsense.cc
+++ lab1/nonsense.cc
@@ -8,17 +8,40 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+/**
+* Error version
+*/
+// int main(int argc, char *argv[]) {
+//   int fd1 = open("nonsense.cc", O_RDONLY);
+//   int fd2 = dup(fd1);
+//   close(fd1);
+//   int fd3 = dup(fd2);
+//   int fd4 = dup(fd3);
+//   dup(fd4);
+//   dup2(STDERR_FILENO, fd4);
+//   dup2(STDERR_FILENO, STDOUT_FILENO);
+//   close(STDIN_FILENO);
+//   open("nonsense.cc", O_RDONLY);
+//   close(STDERR_FILENO);
+//   return 0;
+// }
 int main(int argc, char *argv[]) {
   int fd1 = open("nonsense.cc", O_RDONLY);
   int fd2 = dup(fd1);
   close(fd1);
   int fd3 = dup(fd2);
   int fd4 = dup(fd3);
-  dup(fd4);
+  int dupfd4 = dup(fd4);
   dup2(STDERR_FILENO, fd4);
-  dup2(STDERR_FILENO, STDOUT_FILENO);
+  int dupstd = dup2(STDERR_FILENO, STDOUT_FILENO);
   close(STDIN_FILENO);
-  open("nonsense.cc", O_RDONLY);
+  int openafile = open("nonsense.cc", O_RDONLY);
   close(STDERR_FILENO);
+  close(fd4);
+  close(dupfd4);
+  close(fd3);
+  close(fd2);
+  close(dupstd);
+  close(openafile);
   return 0;
 }
